5bd1a3531587758364378cb32c9b190bb4ab94a2
Ramon <ramon@eeza.csic.es>
2022-08-29 07:43:31 +0200

    Add IMapper interface

---
+++ Rop.Mapper/DefaultMapper.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Concurrent;
+using Rop.Mapper.Converters;
+using Rop.Mapper.Rules;
+
+namespace Rop.Mapper;
+
+public static class DefaultMapper
+{
+    internal static readonly ConcurrentDictionary<string, IConverter> DefaultConvertersDic;
+    private static readonly Mapper _defaultmapper;
+    public static Dst MapTo<Dst>(this IMappeable item) where Dst : class, new() => _defaultmapper.Map<Dst>(item);
+    public static void MapTo<Dst>(this IMappeable item, Dst destiny) where Dst : class => _defaultmapper.Map(item, destiny);
+
+    public static IEnumerable<Dst> MapEnumerable<Dst>(this IEnumerable<IMappeable> item) where Dst : class, new() => _defaultmapper.MapEnumerable<Dst>(item);
+        
+    public static Dst Map<Dst>(object item) where Dst : class, new()
+    {
+        return _defaultmapper.Map<Dst>(item);
+    }
+    public static void Map<Dst>(object item, Dst destiny) where Dst : class
+    {
+        _defaultmapper.Map<Dst>(item, destiny);
+    }
+
+    internal static string GetKeyConverter(Type src, Type dst)
+    {
+        var key = $"{src.Name}|{dst.Name}";
+        return key;
+    }
+    static DefaultMapper()
+    {
+        DefaultConvertersDic = new ConcurrentDictionary<string, IConverter>(StringComparer.OrdinalIgnoreCase)
+        {
+            [GetKeyConverter(typeof(DateTime), typeof(DateOnly))] = new DateOnlyConverter(),
+            [GetKeyConverter(typeof(DateOnly), typeof(DateTime))] = new DateOnlyConverter(),
+
+            [GetKeyConverter(typeof(TimeSpan), typeof(TimeOnly))] = new TimeOnlyConverter(),
+            [GetKeyConverter(typeof(TimeOnly), typeof(TimeSpan))] = new TimeOnlyConverter(),
+            [GetKeyConverter(typeof(DateTime), typeof(TimeOnly))] = new DateTimeToTimeConverter(),
+        };
+        _defaultmapper = new Mapper();
+    }
+}
---
+++ Rop.Mapper/IMapper.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using Rop.Mapper.Rules;
+
+namespace Rop.Mapper;
+
+public interface IMapper
+{
+    Dst Map<Dst>(object item) where Dst:class,new();
+    void Map<Dst>(object item,Dst destiny) where Dst:class;
+    IEnumerable<Dst> MapEnumerable<Dst>(IEnumerable items) where Dst : class, new();
+    IConverter? GetConverter(string convertername);
+    IConverter? GetConverter(Type src,Type dst);
+    void RegisterConverter(IConverter converter, bool force=false);
+    void RegisterConverter(Type src,Type dst, IConverter converter, bool force=false);
+}
